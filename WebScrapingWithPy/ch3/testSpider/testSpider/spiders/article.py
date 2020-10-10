6a755e4fdb22f4359b1dfb8e15e262a2baf8cffa
Holmze <704062177@qq.com>
2020-10-10 14:45:09 +0800

    updatez scrapy

---
+++ WebScrapingWithPy/ch3/testSpider/testSpider/spiders/article.py
@@ -0,0 +1,20 @@
+import scrapy
+
+class ArticleSpider(scrapy.Spider):
+    name = 'article'
+
+    def start_requests(self):
+        urls = ['https://cn.bing.com/?FORM=Z9FD1'
+        ,'https://edu.cnblogs.com/campus/fzu/SE2020/homework/11277'
+        ,'https://www.cnblogs.com/easteast/'
+        ,'https://github.com/Holmze/031804103-051806129'
+        ]
+        return [scrapy.Request(url = url,callback = self.parse) for url in urls]
+
+    def parse(self,response):
+        url = response.url
+        title = response.css('title::text').extract_first()
+        print("======================================")
+        print('URL is: {}'.format(url))
+        print('Title is: {}'.format(title))
+        print("======================================")
