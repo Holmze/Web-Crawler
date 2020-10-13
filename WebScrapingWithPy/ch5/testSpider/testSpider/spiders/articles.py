6daa80eb5c75b3cd69a8182496973e29311d0041
Holmze <704062177@qq.com>
2020-10-13 22:39:06 +0800

    update

--- WebScrapingWithPy/ch5/testSpider/testSpider/spiders/articles.py
+++ WebScrapingWithPy/ch5/testSpider/testSpider/spiders/articles.py
@@ -3,12 +3,18 @@ from scrapy.spiders import CrawlSpider,Rule
 
 class ArticleSpider(CrawlSpider):
     name = 'articles'
-    allowed_domains = ['cnblogs.com']
-    start_urls = ['https://www.cnblogs.com/holmze/']
+    print(name)
+    allowed_domains = ['www.cnblogs.com']
+    print(name)
+    start_urls = ['https://www.cnblogs.com/holmze/p/13797920.html']
+    print(start_urls)
     rules = [Rule(LinkExtractor(allow = r'.*'),callback = 'parse_items',follow = True)]
+    print(rules)
 
     def parse_items(self,response):
+        print(response)
         url = response.url
+        print(url)
         title = response.css('title::text').extract_first()
         text = response.xpath('//div[@id="post_list"]//text()').extract()
         # lastUpdated = response.css('li#footer-info-lastmod::text').extract_first()
