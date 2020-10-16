88f5988c418d2691cfeb7a7c85743777dc228415
Holmze <704062177@qq.com>
2020-10-16 17:14:25 +0800

    finish WebPy ch5

---
+++ WebScrapingWithPy/ch5/cnblogSpider/cnblogSpider/articlePiplines.py
@@ -0,0 +1,28 @@
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider,Rule
+from cnblogSpider.items import Article
+
+class ArticleSpider(CrawlSpider):
+    name = 'articlesPiplines'
+    allowed_domains = ['www.cnblogs.com']
+    start_urls = ['https://www.cnblogs.com/holmze/']
+    # start_urls = ['https://www.cnblogs.com/holmze/p/13797920.html']
+    ## 使用re匹配，爬取cnblog上的所有个人博客页面
+    rules = [Rule(LinkExtractor(allow = r'.*holmze\/p.*'),callback = 'parse_items',follow = True)]
+
+    def parse_items(self,response):
+        article = Article()
+        article['url'] = response.url
+        # print(url)
+        article['title'] = response.css('title::text').extract_first()
+        # text = response.css('div#cnblogs_post_body::text').extract_first()
+        article['text'] = response.xpath('//div[@id="cnblogs_post_body"]//text()').extract()
+        article['lastUpdated'] = response.css('span#post-date::text').extract_first()
+        # print(article['lastUpdated'])
+        # print(article['lastUpdated'],":",article['title'])
+        # lastUpdated = lastUpdated.replace('This page was last edited on ','')
+        # print('URL is: {}'.format(url))
+        # print('title is: {} '.format(title))
+        # print('text is: {}'.format(text))
+        # print('Last updated: {}'.format(lastUpdated))
+        return article
--- WebScrapingWithPy/ch5/cnblogSpider/cnblogSpider/pipelines.py
+++ WebScrapingWithPy/ch5/cnblogSpider/cnblogSpider/pipelines.py
@@ -5,9 +5,27 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+# from itemadapter import ItemAdapter
 
 
-class CnblogspiderPipeline:
-    def process_item(self, item, spider):
-        return item
+# class CnblogspiderPipeline:
+#     def process_item(self, item, spider):
+#         return item
+
+from datetime import datetime
+from cnblogSpider.items import Article
+from string import whitespace
+
+class CnblogspiderPipeline(object):
+    def process_item(self,article,spider):
+        dateStr = article['lastUpdated']
+        # if article['lastUpdated'] is not None:
+        #     article['lastUpdated'] = article['lastUpdated'].strip()
+        #     article['lastUpdated'] = datetime.strptime(article['lastUpdated'],'%Y-%B-%d %H:%M')
+        # print(dateStr)
+        article['text'] = [line for line in article['text'] if line not in whitespace]
+        article['text'] = ''.join(article['text'])
+        article['title'] = str(article['title']).split('-')[0]
+        print(article['lastUpdated'],":",article['title'],"in",article['url'])
+        print(article['text'])
+        return article
